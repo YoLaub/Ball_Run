ce3aed6afa45b340dce17086bc2a7cc0749ece3a
YoLaub <johngreta904@gmail.com>
2025-01-15 22:42:02 +0100

    optmisation

--- script/ballRun.js
+++ script/ballRun.js
@@ -3,15 +3,26 @@ import { initializeAudio, detectBeats, drawB, getCurrentFrequency } from "./audi
 var canva = document.getElementById("gameCanvas");
 const context = canva.getContext("2d");
 
+var modeTest = true;
+
 //Gestion du score
 var score = 0;
 var scoreDisplay = 0;
+var scoreTab = [];
 
 //Debut de partie
 var start = false;
 
 //Fin de partie
 var isGameOver = false;
+var gameOver = 0;
+var animationId = null;
+var endGame = false;
+// Variables pour gérer le message de fin
+let messageFinActive = false;
+let messageFinX = canva.width; // Point de départ à droite de l'écran
+const messageFinText = "Fin";
+const messageFinSpeed = 2;
 
 //Gestion de la balle
 var ballY = 290;
@@ -102,9 +113,13 @@ document.addEventListener("keyup", (e) => {
   }
 });
 
-const backgroundMusic = new Audio("musique/test3.mp3");
-backgroundMusic.loop = true; // Répète la musique en boucle
+const backgroundMusic = new Audio("musique/test.mp3");
 backgroundMusic.volume = 1; // Ajuste le volume
+
+backgroundMusic.addEventListener('ended', () => {
+  messageFinActive = true
+  console.log("ended")
+  })
 
 //Generer les bonus
 function generateBonus() {
@@ -191,34 +206,11 @@ function updateSlow() {
     }
   }
 }
-/*
-  function drawScrollingBackground() {
-    // Dessiner les deux images côte à côte
-    context.drawImage(bgImage1, bgX1, 0, canva.width, canva.height);
-    context.drawImage(bgImage2, bgX2, 0, canva.width, canva.height);
-  
-    // Déplacer les images vers la gauche
-    if (isSlow) {
-      bgX1 -= baseSpeed * 0.08; 
-      bgX2 -= baseSpeed * 0.08;
-    } else {
-      bgX1 -= baseSpeed * 0.1;
-      bgX2 -= baseSpeed * 0.1;
-    }
-  
-    // Réinitialiser la position pour créer un défilement infini
-    if (bgX1 + canva.width <= 0) {
-      bgX1 = bgX2 + canva.width;
-    }
-    if (bgX2 + canva.width <= 0) {
-      bgX2 = bgX1 + canva.width;
-    }
-  }
-    */
+
 
 //Generer les particules
 function generateParticules() {
-  const particule = { x: canva.width, y: Math.random() * (groundY), width: 3, height: 1 };
+  const particule = { x: Math.random() * 800, y: Math.random() * (groundY), width: 2, height: 1 };
   particules.push(particule)
 
 }
@@ -229,8 +221,10 @@ function drawParticule() {
     let i = particule;
     if (!isSlow) {
       particule.x -= speed * 0.3;
+      particule.y += 0.1;
     } else {
       particule.x -= baseSpeed * 0.3;
+      particule.y += 1;
     }
 
 
@@ -322,14 +316,17 @@ function checkObstaclesCollision() {
       ) {
         console.log("Collision détectée !");
         isGameOver = true; // Déclenche le Game Over
-
+        scoreTab.push(scoreDisplay);
+        gameOver++;
         start = false;
+        backgroundMusic.pause();
         break;
       }
     }
   }
 }
 
+/*
 function drawBackground() {
   backgrounds.forEach((background) => {
     if (background.x + background.width < 0) {
@@ -360,9 +357,28 @@ function drawBackground() {
     }
     /*if (backgroundX2 + canva.width < 0) {
         backgroundX2 = canva.width;
-      }*/
+      }
   });
 }
+*/
+
+// Fonction pour dessiner le message "Fin"
+function drawEndMessage() {
+  if (messageFinActive) {
+    context.font = "48px Arial";
+    context.fillStyle = "black";
+    context.fillText(messageFinText, messageFinX, canva.height / 2);
+    messageFinX -= messageFinSpeed;
+    console.log("hi")
+    // Vérifier si le message est complètement sorti de l'écran
+    if (messageFinX  === canva.height / 2) {
+      console.log("ho")
+      stopGame(); // Arrêter le jeu
+    }
+  }
+}
+
+
 
 function updateGame() {
   if (isGameOver) {
@@ -377,11 +393,12 @@ function updateGame() {
       canva.width / 2,
       canva.height / 2 + 40
     );
-    backgroundMusic.pause();
     return; // Arrête la boucle du jeu
   }
 
   context.clearRect(0, 0, canva.width, canva.height);
+
+ 
 
   //drawScrollingBackground();
   drawB(context, canva);
@@ -421,8 +438,6 @@ function updateGame() {
       velocityY = 0; //Stop le mouvement
     }
   }
-
-
   //Ajuster la vitess en fonction du score
 
   if (speed > 15) {
@@ -432,75 +447,16 @@ function updateGame() {
     speed = baseSpeed + 0.1 * Math.floor(scoreDisplay / 5);
   }
 
-  detectBeats(() => {
-    generateObstacle(); // Génère un nouvel obstacle sur chaque beat détecté
-  });
+  if (!modeTest){
+    detectBeats(() => {
+      generateObstacle(); // Génère un nouvel obstacle sur chaque beat détecté
+    });
+    drawObstacles();
+  }
 
-  drawObstacles();
-
+ 
+  
   checkObstaclesCollision();
-
-  /*
-  //Dessine les obstacles
-  obstacles.forEach((obstacle) => {
-    if (!isSlow) {
-      obstacle.x -= speed;
-    } else {
-      obstacle.x -= baseSpeed;
-    }
-
-    if (obstacle.x + obstacle.width < 0) {
-      obstacle.x = canva.width + Math.floor(Math.random() * 300); // Réinitialise l'obstacle à une position aléatoire
-      obstacle.width = 10 + Math.floor(Math.random() * 50); // Taille aléatoire
-      obstacle.height = 10 + Math.floor(Math.random() * 50);
-      score++;
-    }
-    context.fillStyle = "black";
-    context.fillRect(
-      obstacle.x,
-      groundY - obstacle.height,
-      obstacle.width,
-      obstacle.height
-    );
-
-    // Détection de collision avec le haut du bloc
-    if (!isInvincible) {
-      const ballBottom = ballY + 10;
-      const ballTop = ballY - 10; // Haut de la balle
-      const ballRight = 100 + 10;
-      const ballLeft = 100 - 10;
-
-      const obstacleTop = groundY - obstacle.height;
-      const obstacleLeft = obstacle.x;
-      const obstacleRight = obstacle.x + obstacle.width;
-
-      //collision haut de block
-      if (
-        ballBottom > obstacleTop &&
-        ballTop < obstacleTop + 1 &&
-        ballRight > obstacleLeft &&
-        ballLeft < obstacleRight
-      ) {
-        ballY = obstacleTop - 10; // Place la balle sur le bloc
-        velocityY = 0; // Arrête la gravité
-        isOnBlock = true;
-      } else if (
-        ballRight > obstacleLeft &&
-        ballLeft < obstacleRight &&
-        ballBottom > obstacleTop
-      ) {
-        console.log("Collision détectée !");
-        isGameOver = true; // Déclenche le Game Over
-        start = false;
-      }
-    }
-  });*/
-
-  // Réactiver la gravité si la balle quitte le bloc
-  /*if (!isOnBlock) {
-      velocityY += gravity;
-    }*/
-
   checkBonusCollision();
   updateInvincibility();
   updateSlow();
@@ -515,14 +471,14 @@ function updateGame() {
   }
 
   // Indicateur de bonus
-  if (isInvincible) {
+  if (isInvincible && !isGameOver) {
     context.fillStyle = "black";
     context.font = "40px Arial";
     context.textAlign = "center";
     context.fillText("INVINCIBLE", canva.width / 2, 100);
     context.font = "20px Arial";
   }
-  if (isSlow) {
+  if (isSlow && !isGameOver) {
     context.fillStyle = "black";
     context.font = "40px Arial";
     context.textAlign = "center";
@@ -530,21 +486,21 @@ function updateGame() {
     context.font = "20px Arial";
   }
 
-  if (speed > 10) {
+  if (speed > 10 && !messageFinActive && !isGameOver) {
     context.fillStyle = "black";
     context.font = "40px Arial";
     context.textAlign = "center";
     context.fillText("YOU SO ARE FAST!!", canva.width / 2, canva.height);
     context.font = "20px Arial";
   }
-  if (speed > 5 && speed < 10) {
+  if (speed > 5 && speed < 10 && !messageFinActive && !isGameOver) {
     context.fillStyle = "black";
     context.font = "40px Arial";
     context.textAlign = "center";
     context.fillText("GO FAST!!", canva.width / 2, canva.height);
     context.font = "20px Arial";
   }
-  if (speed < 4) {
+  if (speed < 4 && !messageFinActive && !isGameOver) {
     context.fillStyle = "black";
     context.font = "40px Arial";
     context.textAlign = "center";
@@ -556,7 +512,33 @@ function updateGame() {
   context.font = "16px serif";
   context.fillText("Score: " + scoreDisplay, 600, 20);
 
-  requestAnimationFrame(updateGame);
+  if (!messageFinActive) {
+    animationId = requestAnimationFrame(updateGame);
+  }else {
+    drawEndMessage();
+  }
+
+   // Toujours vérifier et dessiner le message de fin si nécessaire
+
+  
+}
+
+function stopGame() {
+  cancelAnimationFrame(animationId)
+  endGame = true;
+  context.clearRect(0, 0, canva.width, canva.height);
+
+  context.fillStyle = "black";
+  context.font = "40px Arial";
+  context.textAlign = "center";
+  context.fillText("Your best Score: " + (Math.max(...scoreTab)), canva.width / 2, canva.height / 2);
+  context.font = "20px Arial";
+  context.fillText(
+    "You failed " + (gameOver) + " times",
+    canva.width / 2,
+    canva.height / 2 + 40
+  )
+  console.log(scoreTab)
 }
 
 function restartGame() {
